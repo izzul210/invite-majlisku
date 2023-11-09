52a8944e8d7d4c716d5ed851b8b2cf0007310345
Izzul Syahmi <izzulsyahmi210@gmail.com>
2023-11-09 22:18:57 +0800

    implemented image metadata

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -26,11 +26,14 @@ export async function generateMetadata({ params }) {
 	// read route params
 	const id = params.id;
 
+	//init data
 	let title = `You're cordially invited to our Event!`;
 	let description = `Kindly click to RSVP `;
 	let weddingText = '';
+	let imageUrl =
+		'https://firebasestorage.googleapis.com/v0/b/myweddingapp-25712.appspot.com/o/wallpaper%2Fmetadata_img.png?alt=media&token=769f323f-8ad3-4ab3-a742-d45d959b4da2';
 
-	// fetch data
+	//fetch data
 	const rsvpDetails = await fetch(
 		`https://asia-southeast1-myweddingapp-25712.cloudfunctions.net/user/rsvpdetails/${id}`,
 		{ cache: 'no-store' }
@@ -47,9 +50,31 @@ export async function generateMetadata({ params }) {
 
 	title = `${weddingText} | ${eventDate}`;
 
+	if (rsvpDetails?.whatsapp_metadata_img) {
+		imageUrl = rsvpDetails.whatsapp_metadata_img;
+	}
+
+	if (rsvpDetails?.metadata) {
+		if (rsvpDetails?.metadata?.title) {
+			title = `${rsvpDetails.metadata.title} | ${eventDate}`;
+		}
+		if (rsvpDetails?.metadata?.photoURL) {
+			imageUrl = rsvpDetails.metadata.photoURL;
+		}
+	}
+
+	title = title.replace(/\n/g, ' ');
+
 	return {
 		title: title,
 		description: description,
+		images: [
+			{
+				url: imageUrl,
+				width: 1200,
+				height: 630,
+			},
+		],
 	};
 }
 
