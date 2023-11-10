c68515bc1f035b6469f278942ef5c54a7a38f301
Izzul Syahmi <izzulsyahmi210@gmail.com>
2023-11-10 17:19:05 +0800

    removed loading spinner to see if that affects the performance

--- template/SplashScreen.jsx
+++ template/SplashScreen.jsx
@@ -1,18 +1,9 @@
 /** @format */
 'use client';
 import React from 'react';
-import { motion } from 'framer-motion';
-import { MajliskuLoadingIcon } from '../component/icons/icons';
 
 function SplashScreen() {
-	return (
-		<motion.div
-			initial={{ opacity: 1, filter: 'blur(0)' }}
-			animate={{ opacity: 0, filter: 'blur(10px)', transition: { duration: 2, delay: 1 } }}
-			className='loading-overlay'>
-			<MajliskuLoadingIcon />
-		</motion.div>
-	);
+	return <div className='loading-overlay'></div>;
 }
 
 export default SplashScreen;
