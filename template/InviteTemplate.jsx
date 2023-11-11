b7873857d3d3deaa0b2404af7acccc8c06f0168b
Izzul Syahmi <izzulsyahmi210@gmail.com>
2023-11-11 14:33:20 +0800

    added splash screen loading

--- template/InviteTemplate.jsx
+++ template/InviteTemplate.jsx
@@ -1,6 +1,7 @@
 /** @format */
 'use client';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import { useEventDetails, useItineraryList, useWishList } from '../hooks/useApi';
 //Screen import
 import FirstScreen from './firstScreen/FirstScreen';
@@ -9,7 +10,7 @@ import EventDetails from './eventDetails/EventDetails';
 import Tentative from './tentative/Tentative';
 import Wishlist from './wishlist/Wishlist';
 import Contacts from './contacts/Contacts';
-import SplashScreen from './SplashScreen';
+import { MajliskuLoadingIcon } from '../component/icons/icons';
 
 //API import
 
@@ -18,21 +19,42 @@ function InviteTemplate({ inviteId }) {
 	const { data: itinerary } = useItineraryList();
 	const { data: wishlist } = useWishList();
 
-	if (isLoading) return <SplashScreen />;
+	const [loadingAnimation, setLoadingAnimation] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setLoadingAnimation(false);
+		}, 4000);
+		return () => clearTimeout(timer);
+	}, []);
 
 	return (
-		<div className='w-full px-0 pb-6 sm:px-4 h-full flex flex-col items-center pt-0 sm:pt-24 sm:bg-transparent'>
-			<div className='w-full flex flex-col items-center bg-white max-w-md sm:shadow-xl'>
-				<FirstScreen eventDetails={eventDetails} />
-				<GreetingScreen eventDetails={eventDetails} />
-				<EventDetails eventDetails={eventDetails} />
-				<div className='w-full flex gap-3 flex-col px-5 sm:px-0 py-8' style={{ maxWidth: '400px' }}>
-					<Tentative eventDetails={eventDetails} itinerary={itinerary} />
-					<Contacts eventDetails={eventDetails} />
-					<Wishlist eventDetails={eventDetails} wishlist={wishlist} />
+		<>
+			{loadingAnimation && !isLoading ? (
+				<motion.div
+					initial={{ opacity: 1, filter: 'blur(0)' }}
+					animate={{ opacity: 0, filter: 'blur(10px)', transition: { duration: 2, delay: 1 } }}
+					className='loading-overlay'>
+					<MajliskuLoadingIcon />
+				</motion.div>
+			) : null}
+			{!isLoading && (
+				<div className='w-full px-0 pb-6 sm:px-4 h-full flex flex-col items-center pt-0 sm:pt-24 sm:bg-transparent'>
+					<div className='w-full flex flex-col items-center bg-white max-w-md sm:shadow-xl'>
+						<FirstScreen eventDetails={eventDetails} />
+						<GreetingScreen eventDetails={eventDetails} />
+						<EventDetails eventDetails={eventDetails} />
+						<div
+							className='w-full flex gap-3 flex-col px-5 sm:px-0 py-8'
+							style={{ maxWidth: '400px' }}>
+							<Tentative eventDetails={eventDetails} itinerary={itinerary} />
+							<Contacts eventDetails={eventDetails} />
+							<Wishlist eventDetails={eventDetails} wishlist={wishlist} />
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 }
 
