80d3830a025381683277a565917baf81fdb050f1
Izzul Syahmi <izzulsyahmi210@gmail.com>
2023-11-30 22:15:28 -0800

    included metadata in personalized

--- app/[id]/[guestId]/page.js
+++ app/[id]/[guestId]/page.js
@@ -1,5 +1,60 @@
 /** @format */
+import moment from 'moment';
 import InviteTemplateGuest from '../../../template/InviteTemplateGuest';
+
+const API = 'https://asia-southeast1-myweddingapp-25712.cloudfunctions.net/user';
+// const API = 'http://localhost:5000/myweddingapp-25712/asia-southeast1/user';
+
+/**************** Generating Metadata **********/
+export async function generateMetadata({ params }) {
+	// read route params
+	const id = params.id;
+	//init data
+	let title = `You're cordially invited to our Event!`;
+	let description = `Kindly click to RSVP `;
+	let weddingText = '';
+	let imageUrl =
+		'https://firebasestorage.googleapis.com/v0/b/myweddingapp-25712.appspot.com/o/wallpaper%2Fmetadata_img.png?alt=media&token=769f323f-8ad3-4ab3-a742-d45d959b4da2';
+
+	//fetch data
+	const rsvpDetails = await fetch(`${API}/rsvpdetails/${id}`).then((res) => res.json());
+
+	//Filter data
+	if (rsvpDetails?.event_title_2) {
+		weddingText = rsvpDetails.event_title_2;
+	} else if (rsvpDetails?.bride_name && rsvpDetails?.groom_name) {
+		weddingText = `${rsvpDetails.groom_name} & ${rsvpDetails.bride_name}`;
+	}
+
+	const eventDate = moment(rsvpDetails?.event_date).format('DD.MM.YY');
+
+	title = `${weddingText} | ${eventDate}`;
+
+	if (rsvpDetails?.whatsapp_metadata_img) {
+		imageUrl = rsvpDetails.whatsapp_metadata_img;
+	}
+
+	if (rsvpDetails?.metadata) {
+		if (rsvpDetails?.metadata?.title) {
+			title = `${rsvpDetails.metadata.title} | ${eventDate}`;
+		}
+		if (rsvpDetails?.metadata?.photoURL) {
+			imageUrl = rsvpDetails.metadata.photoURL;
+		}
+	}
+
+	title = title.replace(/\n/g, ' ');
+
+	return {
+		title: title,
+		description: description,
+		openGraph: {
+			title: title,
+			description: description,
+			images: imageUrl,
+		},
+	};
+}
 
 export default async function Page({ params }) {
 	return (
