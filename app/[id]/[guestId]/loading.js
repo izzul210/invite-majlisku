d65d6e4cb86a60c6dad1f8cf147b8c8dc588eab8
Izzul Syahmi <izzulsyahmi210@gmail.com>
2024-02-24 15:52:28 -0800

    added loading suspense for preview and personalized guest

---
+++ app/[id]/[guestId]/loading.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from 'react';
+import SplashScreen from '../../../template/SplashScreen';
+
+function loading() {
+	return <SplashScreen />;
+}
+
+export default loading;
